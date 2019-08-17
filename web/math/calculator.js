6ea7217806929b7a6d46194efb6d207263248543
madoka20 <41370038+madoka20@users.noreply.github.com>
2019-08-17 18:18:39 +0800

    vue学习

---
+++ web/math/calculator.js
@@ -0,0 +1,12 @@
+function add(){
+	var num1=parseInt(document.getElementById('number1').value);
+	var num2=parseInt(document.getElementById('number2').value);
+	var re1=num1+num2;
+	document.getElementById("result1").value=re1;
+}
+function mul(){
+	var num3=parseInt(document.getElementById('number3').value);
+	var num4=parseInt(document.getElementById('number4').value);
+	var re2=num3*num4;
+	document.getElementById("result2").value=re2;
+}
