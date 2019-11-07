c63db527b4ac51ce4ba47e89ea52396511381b9d
madoka20 <41370038+madoka20@users.noreply.github.com>
2019-11-07 08:58:53 -0500

    11/7

--- IT342Project/list.php
+++ IT342Project/list.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <title>BootstrapTable</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <style>
     </style>
 </head>
@@ -78,6 +79,12 @@
                         <td>
                             <?php echo $row["retired"]; ?>
                         </td>
+                        <td>
+                            <a href="form_update.html" >Update</a>
+                        </td>
+                        <td>
+                            <a href="" >Delete</a>
+                        </td>
                     </tr>
                     <?php              
     }
@@ -96,4 +103,11 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
+<script>
+
+  
+    function del(){
+
+    }
+</script>
 </html>
