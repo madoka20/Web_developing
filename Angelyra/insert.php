73bf8a037fb0be6e056a9d5fb4b51902e93be4ff
madoka20 <41370038+madoka20@users.noreply.github.com>
2019-11-20 21:44:09 -0500

    success

--- Angelyra/insert.php
+++ Angelyra/insert.php
@@ -1,31 +1,26 @@
 <?php
-    $serverName = "madoka.database.windows.net"; // update me
-    $connectionOptions = array(
-        "Database" => "Angelyra", // update me
-        "Uid" => "madoka", // update me
-        "PWD" => "Clare923%" // update me
-    );
-    //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $picture=$_POST("picture");
-    $productid=$_POST("productid");
-    $title=$_POST("title");
-    $price=$_POST("price");
-    $minimumsize=$_POST("minimumsize");
-    $maximumsize=$_POST("maximumsize");
-    $category=$_POST("category");
-    $color=$_POST("color");
-    $pattern=$_POST("pattern");
-    $textile=$_POST("textile");
-    $brand=$_POST("brand");
+    $connectionInfo = array("UID" => "madoka", "pwd" => "Clare923%", "Database" => "Angelyra", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:madoka.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+if( $conn === false ) {
+     die( print_r( sqlsrv_errors(), true));
+}
+echo 'connect successfully';
+    $picture=$_POST['picture'];
+    $productid=intval($_POST['productid']);
+    $title=$_POST['title'];
+    $price=floatval($_POST['price']);
+    $minimumsize=intval($_POST['minimumsize']);
+    $maximumsize=intval($_POST['maximumsize']);
+    $category=$_POST['category'];
+    $color=$_POST['color'];
+    $pattern=$_POST['pattern'];
+    $textile=$_POST['textile'];
+    $brand=$_POST['brand'];
 
-    $tsql= "insert into fashion (picture,productid,title,price,minimumsize,maximumsize,category,color,pattern,textile,brand) values ('$picture','$productid','$title','$price','$minimumsize','$maximumsize','$category','$color','$pattern','$textile','$brand')";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    // while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    //  echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
-    // }
-    sqlsrv_free_stmt($getResults);
+    $tsql= "insert into fashion (picturelink,productId,title,price,minimumsize,maximumsize,category,color,pattern,textile,brand) values ('$picture','$productid','$title','$price','$minimumsize','$maximumsize','$category','$color','$pattern','$textile','$brand')";
+    sqlsrv_query($conn, $tsql);
+   
+    sqlsrv_close($conn);
+    
 ?>
